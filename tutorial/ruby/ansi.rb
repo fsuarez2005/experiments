50dbda23693b0092634c306342f81ccf4f35d698
Frank Suarez <fsuarez2007@gmail.com>
2025-03-25 15:25:09 -0400

    creating ANSI terminal library in Ruby

--- tutorial/ruby/ansi.rb
+++ tutorial/ruby/ansi.rb
@@ -1,48 +1,22 @@
 #!/usr/bin/env ruby
 
-# ANSI test
 
-require "io/console"
-
-$ANSI_COLOR_RANGE = (0..255)
+# ANSI library
 
 
-def place_char(con = IO.console,char,color)
+$ESCAPE_CODE = "\e"
 
-  # change color
-  con.print "\e[48;5;#{color}m"
 
-  con.print char
-  
-  # reset
-  con.print "\e[0m"
+#Control Sequence Introducer
 
-end
 
-def place_random_char(con = IO.console)
-  
-  (lines,columns) = con.winsize
-  
-  line = rand(0..lines)
-  column = rand(0..columns)
-  
-  con.goto(line,column)
-  
-  
-  place_char(" ",rand($ANSI_COLOR_RANGE))
+def cursor_up()
+  "\e[5A"
   
 end
 
 
+puts cursor_up()
 
-def main
-  con = IO.console
-  con.erase_screen(2)
-  loop {
-    place_random_char(con)
-    sleep 0.001
-  }
-  
-end
 
-main
+
